a5c1f14b81999d763c18d1719e06d1ff4e2f2b0a
Eli-Drew <drew313@live.missouristate.edu>
2022-12-13 22:13:46 -0600

    made a note about dice on combatController

---
+++ src/combat/combatController.py
@@ -0,0 +1,8 @@
+"""
+Author:     Drew Rinker
+Date:       12/13/2022
+
+Lot's to do here. 
+
+**** Erik made a good point. Remember that only 5 red, 5 black, and 3 green dice can be used in a turn.
+"""
